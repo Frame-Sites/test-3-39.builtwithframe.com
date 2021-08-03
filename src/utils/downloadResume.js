439338e580c8175479d69318e0a73326d9463eff
Kieran <kieranderfus@gmail.com>
2021-08-03 01:06:29 -0400

    Initial commit

---
+++ src/utils/downloadResume.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import FileDownload from "js-file-download";
+
+const downloadResume = async(url) => {
+    const getResume = axios.create({
+        baseURL: url,
+        method: 'GET',
+        responseType:'blob'
+    })
+    try{
+        const response = await getResume.get()
+        const data = response.data
+        const file = new Blob([data], { type: "application/pdf" });
+        const fileUrl = URL.createObjectURL(file);
+        window.open(fileUrl);
+        return FileDownload(data, "Resume.pdf")
+    }
+    catch(e){
+        alert("Resume url is invalid")
+    }
+}
+
+export default downloadResume;
